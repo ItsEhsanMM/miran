bc2b0b09ce5ac1c6113881e12e2503dcdf9552ff
ItsEhsanMM <itsehsanmm@gmail.com>
2025-02-25 13:15:37 +0330

    add icon to header

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
    title: "Miran oil",
    description: "Browse your dream!",
+   icons: "/favicon.ico",
 };
 
 export default function RootLayout({
